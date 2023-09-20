605a293c5996a31bfc933c17b95cb8ed57e2d089
Angelo <ljy6037@gmail.com>
2023-09-20 16:20:05 +0900

    first commit

---
+++ src/main/java/com/example/spring230920/Spring230920Application.java
@@ -0,0 +1,13 @@
+package com.example.spring230920;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring230920Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring230920Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring230920/Spring230920ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring230920;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring230920ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

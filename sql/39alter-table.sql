9f41c2f3c8e07e54f764aea9299f110406cf789f
Angelo <ljy6037@gmail.com>
2023-10-16 12:31:21 +0900

    231016 alter table

---
+++ sql/39alter-table.sql
@@ -0,0 +1,45 @@
+USE w3schools;
+
+-- ALTER TABLE : 테이블 수정
+
+-- 컬럼 추가, 변경, 삭제
+-- 제약사항 변경
+-- NOT NULL, UNIQUE, PRIMARY KEY, DEFAULT, FOREIGN KEY
+
+-- 컬럼 추가
+ALTER TABLE products
+ADD COLUMN col1 INT;
+ALTER TABLE products
+ADD COLUMN col2 VARCHAR(10);
+
+ALTER TABLE products
+ADD COLUMN col3 VARCHAR(10) AFTER ProductName;
+
+ALTER TABLE products
+ADD COLUMN col4 VARCHAR(10) FIRST;
+
+ALTER TABLE products
+ADD COLUMN col5 INT
+    NOT NULL DEFAULT 1 REFERENCES employees(EmployeeID);
+
+DESC products;
+
+-- 예) 직원 테이블에 salary 컬럼 마지막에 추가
+-- data type 은 dec(10, 2), not null, 기본값은 0.00
+ALTER TABLE employees
+ADD COLUMN Salary DEC(10, 2) NOT NULL DEFAULT 0.00;
+
+DESC employees;
+
+-- 컬럼 삭제
+ALTER TABLE products
+DROP COLUMN col1;
+
+-- 예) product 테이블에서 col2 삭제
+ALTER TABLE products
+    DROP COLUMN col2;
+DESC products;
+
+-- 컬럼 변경(type)
+ALTER TABLE products
+MODIFY COLUMN col3 INT;
